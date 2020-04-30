93b56b00907a7dfd121b20aebad7b9e5922f8a73
Andrew Dearden <andrew@andrewdearden.com>
2020-04-30 16:44:34 +0100

    Add project files.

---
+++ ActionExecution/ActionExecution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace ActionExecution
+{
+    public class ActionExecution
+    {
+        private readonly ConcurrentQueue<string> _actionQueue;
+        public ActionExecution()
+        {
+            _actionQueue = new ConcurrentQueue<string>();
+        }
+
+        public void StartProcessing()
+        {
+         
+            while (!_actionQueue.IsEmpty)
+            {
+                ProcessNextAction();                
+            }
+
+        }
+
+        public void AddAction(string singleTask)
+        {
+            _actionQueue.Enqueue(singleTask);
+        }
+
+        public async void ProcessNextAction()
+        {
+            _actionQueue.TryDequeue(out var currentAction);
+            var syncAction = new Task(() => ProcessAction(currentAction));
+            syncAction.RunSynchronously();
+            await Task.Yield();
+
+        }
+        
+        private static async void ProcessAction(string action)
+        {
+            Console.WriteLine(action);
+            await Task.Yield();
+        }
+
+        public bool ActionsToBeProcessed()
+        {
+            return _actionQueue.Count > 0;
+        }
+
+    }
+}
---
+++ ActionExecution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace ActionExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actionExecution = new ActionExecution();
+
+            foreach (var single in Enumerable.Range(1, 1000))
+            {
+                actionExecution.AddAction($"Action {single}");
+            }
+
+            actionExecution.StartProcessing();
+
+            actionExecution.AddAction($"Action 1001");
+            actionExecution.AddAction($"Action 1002");
+
+            if(actionExecution.ActionsToBeProcessed())
+            {actionExecution.StartProcessing();}
+        }
+    }
+}
